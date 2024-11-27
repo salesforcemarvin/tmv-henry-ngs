984493ebed7246ced8277a96c5c0a929bc56d436
Henry Harion <hariondeveloper@gmail.com>
2024-11-27 07:25:27 +0700

    ok

---
+++ index.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const axios = require("axios"); // trigger api call
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+const config = require("./public/config.json");
+app.post("/save", function (req, res)  {
+  // Handle save request
+  console.log("SAVE REQUEST");
+  console.log(req.body);
+  res.sendStatus(200);
+});
+app.post("/publish", function (req, res)  {
+  // Handle publish request
+  res.sendStatus(200);
+});
+app.post("/validate", function (req, res) {
+  // Handle validate request
+  console.log("VALIDATE REQUEST");
+  console.log(req.body);
+  res.sendStatus(200);
+});
+app.post("/stop", async function (req, res) {
+  console.log("STOPPING JOURNEY");
+  console.log(req.body);
+  res.send("Done");
+});
+
+app.post("/execute", async function (req, res) {
+  // Endpoint to handle the execution of the custom activity
+
+  console.log("RUNNING CUSTOM ACTIVITY HERE");
+  console.log("-------REQUEST------");
+  
+  let channel = "@vcbsalesforce";
+  let contact = "632717898";
+  const token = "7622096585:AAHe3Tdc4zsc9-9hKvY0C5briAUo4QSIUWs";
+  const endpoint = "https://api.telegram.org/bot";
+  const url = `${endpoint}${token}/`;
+
+  // console.log(config);
+  try {
+    try {
+      // const response = await axios.post("https://api.example.com/endpoint", {
+      //   data: req.body,
+      // });
+
+      //let contactKey = req.body.keyValue
+      let inArguments = req.body.inArguments
+
+      // console.log(inArguments.length);
+      // console.log(inArguments[0]['chat_id']);
+      // console.log(inArguments[1]['emailAddress']);
+      // console.log(inArguments[2]['customMessage']);
+
+      const chat_id = inArguments[0]['chat_id'];
+      const emailAddress = inArguments[1]['emailAddress'];
+      const customMessage = inArguments[2]['customMessage'];
+
+      const response = await axios.get(
+        `${url}sendMessage?chat_id=${chat_id}&text=${emailAddress}`
+      );
+
+      res.send(response.data);
+    } catch (error) {
+      console.error("Error triggering API call:", error);
+      res.status(500).send("Error triggering API call");
+    }
+
+    res.status(200).send({ status: "success" });
+  } catch (error) {
+    console.error("Error executing custom activity:", error);
+    res.status(500).send({
+      error: "An error occurred while executing the custom activity. 555",
+    });
+  }
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+//for testing
+// const data = {
+//   execute: {
+//     inArguments: [
+//       {
+//         contactKey: "LACUNA01",
+//         emailAddress: "MMMM@gmail.com",
+//       },
+//     ]
+//   },
+// };
+
+// const fs = require("fs");
+// fs.readFile(".public/config.json", "utf8", (err, data) => {
+//   if (err) {
+//     console.error("Error reading the file:", err);
+//     return;
+//   }
+//   const config = JSON.parse(data);
+//   console.log(config);
+// });
---
+++ public/customActivity.js
@@ -0,0 +1,235 @@
+define(["postmonger"], function (Postmonger) {
+  "use strict";
+
+  var connection = new Postmonger.Session();
+  var payload = {};
+  var lastStepEnabled = false;
+  var steps = [
+    // initialize to the same value as what's set in config.json for consistency
+    { label: "Step 1", key: "step1" },
+    { label: "Step 2", key: "step2" },
+    { label: "Step 3", key: "step3" },
+    { label: "Step 4", key: "step4", active: false },
+  ];
+  var currentStep = steps[0].key;
+
+  //Startup Sequence
+  $(window).ready(onRender);
+
+  connection.on("initActivity", initialize);
+  connection.on("requestedTokens", onGetTokens);
+  connection.on("requestedEndpoints", onGetEndpoints);
+
+  connection.on("clickedNext", onClickedNext);
+  connection.on("clickedBack", onClickedBack);
+  connection.on("gotoStep", onGotoStep);
+
+  function onRender() {
+    // JB will respond the first time 'ready' is called with 'initActivity'
+    connection.trigger("ready");
+
+    connection.trigger("requestTokens");
+    connection.trigger("requestEndpoints");
+
+    var message = getMessage();
+    $("#message").html(message);
+  }
+
+  function initialize(data) {
+    // document.getElementById("configuration").value = JSON.stringify(
+    //   data,
+    //   null,
+    //   2
+    // );
+
+    console.log("-------- triggered:onInitActivity({obj}) --------");
+    if (data) {
+      payload = data;
+    }
+
+    var message;
+    var hasInArguments = Boolean(
+      payload["arguments"] &&
+        payload["arguments"].execute &&
+        payload["arguments"].execute.inArguments &&
+        payload["arguments"].execute.inArguments.length > 0
+    );
+
+    var inArguments = hasInArguments
+      ? payload["arguments"].execute.inArguments
+      : {};
+
+    $.each(inArguments, function (index, inArgument) {
+      $.each(inArgument, function (key, val) {
+        if (key === "message") {
+          message = val;
+        }
+      });
+    });
+
+    // If there is no message selected, disable the next button
+    if (!message) {
+      showStep(null, 1);
+      connection.trigger("updateButton", { button: "next", enabled: true });
+      //connection.trigger("updateButton", { button: "next", enabled: false });
+      // If there is a message, skip to the summary step
+    } else {
+      document.getElementById("configuration").value = message;
+      $("#message").html(message);
+      //showStep(null, 3);
+    }
+  }
+
+  function onGetTokens(tokens) {
+    // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
+    // console.log(tokens);
+  }
+
+  function onGetEndpoints(endpoints) {
+    // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
+    // console.log(endpoints);
+  }
+
+  //Save Sequence
+  function onClickedNext() {
+    // var configuration = JSON.parse(
+    //   document.getElementById("configuration").value
+    // );
+    //var configuration = document.getElementById("configuration").value;
+
+    //connection.trigger("updateActivity", configuration);
+    //save(configuration);
+
+    if (
+      (currentStep.key === "step3" && steps[3].active === false) ||
+      currentStep.key === "step4"
+    ) {
+      save();
+    } else {
+      connection.trigger("nextStep");
+    }
+  }
+
+  function onClickedBack() {
+    connection.trigger("prevStep");
+  }
+
+  function onGotoStep(step) {
+    showStep(step);
+    connection.trigger("ready");
+  }
+
+  function showStep(step, stepIndex) {
+    if (stepIndex && !step) {
+      step = steps[stepIndex - 1];
+    }
+
+    currentStep = step;
+
+    $(".step").hide();
+
+    switch (currentStep.key) {
+      case "step1":
+        $("#step1").show();
+        connection.trigger("updateButton", {
+          button: "next",
+          enabled: true, //Boolean(getMessage()),
+        });
+        connection.trigger("updateButton", {
+          button: "back",
+          visible: false,
+        });
+        break;
+      case "step2":
+        $("#step2").show();
+        connection.trigger("updateButton", {
+          button: "back",
+          visible: true,
+        });
+        connection.trigger("updateButton", {
+          button: "next",
+          text: "next",
+          visible: true,
+        });
+        break;
+      case "step3":
+        $("#step3").show();
+        connection.trigger("updateButton", {
+          button: "back",
+          visible: true,
+        });
+        if (lastStepEnabled) {
+          connection.trigger("updateButton", {
+            button: "next",
+            text: "next",
+            visible: true,
+          });
+        } else {
+          connection.trigger("updateButton", {
+            button: "next",
+            text: "done",
+            visible: true,
+          });
+        }
+        break;
+      case "step4":
+        $("#step4").show();
+        break;
+    }
+  }
+
+  function save() {
+    var value = getMessage();
+
+    // 'payload' is initialized on 'initActivity' above.
+    // Journey Builder sends an initial payload with defaults
+    // set by this activity's config.json file.  Any property
+    // may be overridden as desired.
+    payload.name = value; //text message to send to telegram
+
+    payload["arguments"].execute.inArguments = [{ message: value }];
+    //payload["arguments"].execute.inArguments.push({"text": value})
+
+    payload["arguments"].execute.inArguments = [
+      {
+        'chat_id': "@vcbsalesforce"
+      },
+      {
+        'emailAddress': '{{InteractionDefaults.Email}}'
+      },
+      {
+        'customMessage': value
+      }
+    ];
+
+    //payload["arguments"].execute.inArguments = [{ "chat_id": "@vcbsalesforce", "text": value }];
+
+    payload["metaData"].isConfigured = true;
+
+
+    //console.log("LOG AGAIN" + payload);
+    let inArguments = payload.arguments.execute.inArguments;
+
+    // get the option that the user selected and save it to
+    console.log("------------ triggering:updateActivity({obj}) ----------------");
+    console.log("Sending message back to updateActivity");
+    console.log("saving\n", value);
+
+    connection.trigger("updateActivity", payload);
+
+    console.log(inArguments.length);
+    console.log(inArguments[0]['chat_id']);
+    console.log(inArguments[1]['emailAddress']);
+    console.log(inArguments[2]['customMessage']);
+    //console.log(value);
+  }
+
+  function getMessage() {
+    return document.getElementById("configuration").value;
+  }
+
+  $("#submit-this").click(function (e) {
+    console.log("test this");
+    save();
+  });
+});
